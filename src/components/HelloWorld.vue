20443c4ceaec4112fb9376e17f988e80698cc75d
JS <csdnum0@gmail.com>
2024-03-31 06:17:08 +0900

    adf

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-bind:class="isActive[0]">Item 1</li>
+      <li :class="isActive[1]">Item 2</li>
+      <li :class="isActive[2]">Item 3</li>
+    </ul>
+  </div>
+</template>
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue';
 
-defineProps({
-  msg: String,
-})
+const isActive = ref(['inactive', 'active', 'inactive', 'inactive']);
 
-const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
+<style>
+.active {
+  color: green;
+}
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
+.inactive {
+  color: red;
 }
 </style>
